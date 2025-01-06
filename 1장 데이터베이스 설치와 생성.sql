a2bc0ab7f29c425639560a09b003f69829809d93
khg0418 <rlagyrud222@gmail.com>
2025-01-06 17:12:27 +0900

    add ch01

---
+++ 1장 데이터베이스 설치와 생성.sql
@@ -0,0 +1,17 @@
+#날짜: 2025/01/06
+#이름: 김효경
+#내용: 1장 데이터베이스 설치와 생성
+
+#실습 1-1
+CREATE DATABASE `StudyDB`;
+DROP DATABASE `StudyDB`;
+
+#실습 1-2
+CREATE USER 'rlagyrud222'@'%' identified by '1234'
+GRANT ALL PRIVILEGES ON StudyDB.* to 'rlagyrud222'@'%';
+FLUSH PRIVILEGES;
+
+#실습 1-3
+ALTER USER 'rlagyrud222'@'%' IDENTIFIED BY 'abc1234';
+DROP USER 'rlagyrud222'@'%';
+FLUSH PRIVILEGES;
